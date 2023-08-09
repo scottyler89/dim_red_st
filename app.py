4d5e0b98a9969251bc1b44d693026263ce89d3f2
Scott Tyler <scottyler89@gmail.com>
2023-08-09 09:50:31 -0400

    added empty wait message

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import skdim
 # Sidebar for setting parameters
 st.sidebar.header("Set Experiment Parameters")
 n_obs = st.sidebar.select_slider("Number of observations:", options=[
-                                 int((1+i)*1000) for i in range(10)])
+                                 int((1+i)*1000) for i in range(4)])
 true_dims = st.sidebar.slider("Number of true dimensions:", 1, 10, 2)
 n_redundant_per_true = st.sidebar.slider(
     "Number of redundant dimensions per true dimension:", 1, 200, 100)
@@ -31,17 +31,24 @@ n_redundant_per_true = st.sidebar.slider(
 # let the user define the sd ratios
 # A session state to keep track of the added values
 if 'sd_ratios' not in st.session_state:
-    st.session_state.sd_ratios = []
+    st.session_state.sd_ratios = [0.01,0.05,.25,1.0]
 
 # Allow the user to input a floating-point value within the range
 new_sd_ratio = st.sidebar.number_input(
-    "Add an SD Ratio:", min_value=0.01, max_value=1.0, step=0.01, value=0.01)
+    "Add an SD Ratio (interesting range is 0.01-1.0):",
+    min_value=0.01, max_value=5.0, step=0.01, value=0.01)
 
 # Button to add the entered value to the list
 if st.sidebar.button("Add SD Ratio"):
     st.session_state.sd_ratios.append(new_sd_ratio)
-    st.session_state.sd_ratios = sorted(list(set(
-        st.session_state.sd_ratios)))  # Sort the values
+    st.session_state.sd_ratios = list(
+        map(float,sorted(list(set(
+        st.session_state.sd_ratios)))))  # Sort the values
+
+# Button to clear the SD Ratios
+if st.sidebar.button("Clear SD Ratios"):
+    st.session_state.sd_ratios = []
+
 
 # Display the current SD Ratios
 st.sidebar.write("Selected SD Ratios:")
@@ -53,14 +60,22 @@ st.sidebar.write(st.session_state.sd_ratios)
 #######################
 
 
-separation_vect = st.sidebar.select_slider(
+sep = st.sidebar.select_slider(
     "Separation Vector:", options=[0, 2, 4, 6, 8])
 
 # Button to run the experiment
 run_button = st.sidebar.button("Run Experiment")
 
+# Create a placeholder for the running message
+running_message = st.empty()
+
+
 # Display the results when the button is pressed
 if run_button:
+    # Display the running message
+    running_message.write(
+        "The experiment is running. This may take several minutes depending on the parameters...")
+
     # Setting the random seed
     np.random.seed(123456)
 
@@ -73,50 +88,53 @@ if run_button:
     sd_lookup = {}
 
     # Iterating through separation values
-    for sep in separation_vect:
-        sep_name = "Clust Sep:" + str(sep)
-        for sd_ratio in st.session_state.sd_ratios:
-            sd_name = "SD ratio:" + str(sd_ratio)
-            sd_lookup[sd_name] = sd_ratio
-            final_dims = true_dims
 
-            # Generate data
-            true_dim_data, obs_data = generate_data(
-                n_obs, true_dims, n_redundant_per_true, true_gen_func, redundant_gen_noise_func, sd_ratio, separation=sep)
+    sep_name = "Clust Sep:" + str(sep)
+    for sd_ratio in st.session_state.sd_ratios:
+        sd_ratio = float(sd_ratio)
+        sd_name = "SD ratio:" + str(sd_ratio)
+        sd_lookup[sd_name] = sd_ratio
+        final_dims = true_dims
 
-            # Estimate intrinsic dimensionality
-            danco = skdim.id.DANCo().fit(obs_data)
-            intrinsic_dim_estimate_dict[sd_name] = danco.dimension_
+        # Generate data
+        true_dim_data, obs_data = generate_data(
+            n_obs, true_dims, n_redundant_per_true, true_gen_func, redundant_gen_noise_func, sd_ratio, separation=sep)
 
-            # Log the data
-            true_dim_dict[sd_name] = true_dim_data
-            obs_data_dict[sd_name] = obs_data
+        # Estimate intrinsic dimensionality
+        danco = skdim.id.DANCo().fit(obs_data)
+        intrinsic_dim_estimate_dict[sd_name] = danco.dimension_
 
-            # Perform dimension reduction
-            dim_red_funcs = [pca_wrapper, nmf_wrapper,
-                             tsne_wrapper, umap_wrapper, som_wrapper]
-            dim_red_names = ["PCA", "NMF", "tSNE", "UMAP", "SOM"]
-            results_dict[sd_name] = dim_reduction(
-                obs_data, dim_red_funcs, dim_red_names, final_dims)
+        # Log the data
+        true_dim_dict[sd_name] = true_dim_data
+        obs_data_dict[sd_name] = obs_data
 
-        # Call the plotting functions
-        st.write(f"### Results for Separation = {sep}")
-        st.write("#### True Dimensions with Noise vs Dimension Reduction")
-        fig1 = plot_dim_reductions(true_dim_dict, results_dict, "")
-        st.pyplot(fig1)
+        # Perform dimension reduction
+        dim_red_funcs = [pca_wrapper, nmf_wrapper,
+                            tsne_wrapper, umap_wrapper, som_wrapper]
+        dim_red_names = ["PCA", "NMF", "tSNE", "UMAP", "SOM"]
+        results_dict[sd_name] = dim_reduction(
+            obs_data, dim_red_funcs, dim_red_names, final_dims)
 
-        st.write("#### Heatmap and Scatter Plots")
-        fig2 = plot_obs_data_heatmap(
-            true_dim_dict, obs_data_dict, intrinsic_dim_estimate_dict, "")
-        st.pyplot(fig2)
+    # Clear the running message placeholder
+    running_message.empty()
+    # Call the plotting functions
+    st.write(f"### Results for Separation = {sep}")
+    st.write("#### True Dimensions with Noise vs Dimension Reduction")
+    fig1 = plot_dim_reductions(true_dim_dict, results_dict, "")
+    st.pyplot(fig1)
 
-        st.write("#### Intrinsic Dimensionality vs Noise Level")
-        fig3 = plot_intrinsic_dimensionality(
-            sd_lookup, intrinsic_dim_estimate_dict, "")
-        st.pyplot(fig3)
+    st.write("#### Heatmap and Scatter Plots")
+    fig2 = plot_obs_data_heatmap(
+        true_dim_dict, obs_data_dict, intrinsic_dim_estimate_dict, "")
+    st.pyplot(fig2)
 
-        st.write("#### Distance Correlations")
-        fig4 = plot_distance_correlations(true_dim_dict, results_dict, "")
-        st.pyplot(fig4)
+    st.write("#### Intrinsic Dimensionality vs Noise Level")
+    fig3 = plot_intrinsic_dimensionality(
+        sd_lookup, intrinsic_dim_estimate_dict, "")
+    st.pyplot(fig3)
 
-        sep_dict[sep_name] = results_dict
+    st.write("#### Distance Correlations")
+    fig4 = plot_distance_correlations(true_dim_dict, results_dict, "")
+    st.pyplot(fig4)
+
+    sep_dict[sep_name] = results_dict
